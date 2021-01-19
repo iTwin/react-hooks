a002dbf25c2f76ad9094c0e941b9b7d77df49609
Michael Belousov <michael.belousov98@gmail.com>
2021-01-19 15:48:28 -0500

    complete barrel file as module main entrypoint

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export {
+  useOnMount,
+  useOnMountInRenderOrder,
+  useOnUnmount,
+  useStable,
+} from "./basic-hooks";
+export { makeContextWithProviderRequired } from "./context-utils";
+export { default as useAsyncEffect } from "./useAsyncEffect";
+export { default as useAsyncInterval } from "./useAsyncInterval";
+export { default as useInlineComponent } from "./useInlineComponent";
+export { default as useInterval } from "./useInterval";
+export { default as useOnChange } from "./useOnChange";
+export { default as usePropertySetter } from "./usePropertySetter";
+export { default as useValidatedInput } from "./useValidatedInput";
