af9ab784cfd73ab14074c2c77d003a8a9acbd914
Michael Belousov <michael.belousov98@gmail.com>
2021-01-19 16:28:50 -0500

    formatting and more linter setup

--- src/useInlineComponent.ts
+++ src/useInlineComponent.ts
@@ -4,7 +4,11 @@ import { useRef } from "react";
 /** stabilizes references to a component so you can define it inline in a render function
  * do not use while depending on the render scope unless you wrap the culprit in a ref which is still a bad idea
  */
-export const useInlineComponent = <P extends {}, T extends React.ElementType<P>>(comp: T) =>
-  useRef(comp).current;
+export const useInlineComponent = <
+  P extends {},
+  T extends React.ElementType<P>
+>(
+  comp: T
+) => useRef(comp).current;
 
 export default useInlineComponent;
--- src/useInterval.ts
+++ src/useInterval.ts
@@ -23,7 +23,7 @@ export function useInterval(callback: () => void, delay: number | null) {
       const id = setInterval(tick, delay);
       return () => clearInterval(id);
     }
-    return () => { };
+    return () => {};
   }, [delay]);
 }
 
